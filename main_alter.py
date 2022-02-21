7dbb9a807537efa285896d24f110684814922cdb
razered <bhagwat.work@gmail.com>
2022-02-20 20:09:23 -0500

    Fix import

--- main_alter.py
+++ main_alter.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 import torch.backends.cudnn as cudnn
-import transfer
+from models import transfer
 import torchvision.models as models
 
 import logging
